94bd317b686fd8d3a6fc04efec6f6d593335d810
Chloe <hachlo@yahoo.fr>
2021-11-08 10:06:42 +1100

    S27 - WebSocket & Socket.io

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import openSocket from 'socket.io-client';
 
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
@@ -37,7 +38,54 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
+    const socket = openSocket('http://localhost:8080');
+    socket.on('posts', (data) => {
+      const { action, post } = data;
+      if (action === 'create') {
+        this.addPost(post);
+      }
+      if (action === 'update') {
+        this.updatePost(post);
+      }
+      if (action === 'delete') {
+        this.loadPosts();
+      }
+    });
   }
+
+  addPost = (post) => {
+    this.setState(
+      (prevState) => {
+        const updatedPosts = [...prevState.posts];
+        if (prevState.postPage === 1) {
+          if (prevState.posts.length >= itemsPerPage) {
+            updatedPosts.pop();
+          }
+          updatedPosts.unshift(post);
+        }
+        return {
+          posts: updatedPosts,
+          totalPosts: prevState.totalPosts + 1,
+        };
+      },
+      () => {
+        console.log('addPost', this.state);
+      },
+    );
+  };
+
+  updatePost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex((p) => p._id === post._id);
+      if (updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post;
+      }
+      return {
+        posts: updatedPosts,
+      };
+    });
+  };
 
   loadPosts = (direction) => {
     if (direction) {
@@ -142,28 +190,7 @@ class Feed extends Component {
         return res.json();
       })
       .then((resData) => {
-        const post = {
-          _id: resData.post._id,
-          title: resData.post.title,
-          content: resData.post.content,
-          creator: resData.post.creator,
-          createdAt: resData.post.createdAt,
-        };
-        this.setState((prevState) => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex((p) => p._id === prevState.editPost._id);
-            updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < itemsPerPage) {
-            updatedPosts = prevState.posts.concat(post);
-          }
-          return {
-            posts: updatedPosts,
-            isEditing: false,
-            editPost: null,
-            editLoading: false,
-          };
-        });
+        this.setState({ isEditing: false, editPost: null, editLoading: false });
       })
       .catch((err) => {
         console.log(err);
@@ -192,14 +219,6 @@ class Feed extends Component {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Deleting a post failed!');
         }
-        return res.json();
-      })
-      .then((resData) => {
-        console.log(resData);
-        this.setState((prevState) => {
-          const updatedPosts = prevState.posts.filter((p) => p._id !== postId);
-          return { posts: updatedPosts, postsLoading: false };
-        });
       })
       .catch((err) => {
         console.log(err);
